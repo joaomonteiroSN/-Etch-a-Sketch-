43d748df58ab7d21519bb0f78921927f6addc997
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2024-09-10 18:55:23 -0300

    ajustes na geracao do grid

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('.container');
 
+const squareSize = (602/16);
+
 let counter = 0;
 
-while(counter <= 256) {
+while(counter <= 255) {
     const square = document.createElement('div');
     square.classList.add('box-el');
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
     container.appendChild(square);
     counter++;
 }
